456192325041eba12f912e3a300f410f9f84da79
LoudSoftware <contact@loudsoftware.tk>
2018-05-10 14:09:09 -0400

    added useful info to the about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
-
-const grayscaleScript:string = "./assets/js/grayscale.js";
+const grayscaleScript = './assets/js/grayscale.js';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +7,18 @@ const grayscaleScript:string = "./assets/js/grayscale.js";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
   private loadGrayscale: Promise<any>;
 
-  constructor(private renderer:Renderer2){
+  constructor(private renderer: Renderer2) {
 
   }
 
   ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
-    this.renderer.setAttribute(document.querySelector('body'),"id","page-top");
+    // Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    // Add 'implements AfterViewInit' to the class.
+    this.renderer.setAttribute(document.querySelector('body'), 'id', 'page-top');
 
-    //load the grayscale.js after everything else is loaded
+    // load the grayscale.js after everything else is loaded
     this.loadGrayscale = new Promise((resolve) => {
       console.log('resolving promise...');
       this.loadScript();
@@ -29,7 +27,7 @@ export class AppComponent {
 
   private loadScript(): void {
     console.log('load grayscale script ...');
-    let node = document.createElement('script');
+    const node = document.createElement('script');
     node.src = grayscaleScript;
     node.type = 'text/javascript';
     node.async = true;
